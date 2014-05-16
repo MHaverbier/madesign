2df58851952c76d7eeaec72148527e3a45e44846
Ralf Westphal <info@ralfw.de>
2014-05-16 16:00:48 +0200

    LogonProvider implementiert: it.logonprovider.WindowsLogonProvider

--- IssueTriage/source/it.logonprovider/it.logonprovider/WindowsLogonProvider.cs
+++ IssueTriage/source/it.logonprovider/it.logonprovider/WindowsLogonProvider.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using it.contracts;
 
 namespace it.logonprovider
 {
-    public class WindowsLogonProvider
+    public class WindowsLogonProvider : ILogonProvider
     {
+        public string WindowsLogonBestimmen()
+        {
+            var username = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            var i = username.IndexOf("\\");
+            if (i >= 0) username = username.Substring(i + 1);
+            return username;
+        }
     }
 }
