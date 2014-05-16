a3eb7a50eaa332cd54f4efdfa21fd57f302c6e4e
ClaudiaLange <claudia.lange@ma-design.de>
2014-05-16 15:58:44 +0200

    Added some classes.

--- IssueTriage/source/it.PoClient/it.PoClient/App.xaml.cs
+++ IssueTriage/source/it.PoClient/it.PoClient/App.xaml.cs
@@ -13,5 +13,24 @@ namespace it.PoClient
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            PoClientIntegration integration = new PoClientIntegration();
+            UI ui = new UI();
+            IssueMapper mapper = new IssueMapper();
+        }
+    }
+
+    public class UI
+    {
+    }
+
+    public class PoClientIntegration
+    {
+    }
+
+    public class IssueMapper
+    {
     }
 }
