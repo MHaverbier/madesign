2bbdc2fa12d73aea74a44ff7ed5e09c3e204db8a
Frank Lindecke <frank.lindecke@googlemail.com>
2014-05-19 08:35:26 +0200

    Testen der API (MongoDbProvider)
    Testprojekt angelegt (Test ist noch nicht so gut)

---
+++ IssueTriage/source/it.dbprovider/it.dbprovider.test/MongoDbProviderTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using it.contracts;
+using it.DBProvider;
+using NUnit.Framework;
+
+namespace it.dbprovider.test
+{
+    [TestFixture]
+    class MongoDbProviderTest
+    {
+        [Test]
+        public void LesenUndSchreiben()
+        {
+            // Der Test sollte umgeschrieben werden; ich teste hier nur, dass ich etwas reinschreiben und wieder rauslesen kann
+            // Da ich aber die Implementierung kenne, kann ich hier auch einen Whitebox Test machen; d.h. separierte MongoDB benutzen
+            
+            // Frage an Ralf: Wie testen wir solchen externen System eigentlich sauber
+
+            var provider = new MongoDbProvider();
+
+            provider.AlleIssuesLoeschen();
+
+            var issue = new Issue() { Beschreibung = "Test", MelderName = "John Doe" };
+            Assert.That( issue.Id, Is.EqualTo( Guid.Empty ) );
+
+            provider.IssueInDBSchreiben( issue );
+
+            Assert.That( issue.Id, Is.Not.EqualTo( Guid.Empty ) );
+
+            var issues = provider.AllelIssuesLesen();
+
+            Assert.That( issues, Is.Not.Empty );
+            Assert.That( issues.First(), Is.Not.SameAs( issue ) );
+            Assert.That( issues.First().Id, Is.EqualTo( issue.Id ) );
+
+        }
+    }
+}
