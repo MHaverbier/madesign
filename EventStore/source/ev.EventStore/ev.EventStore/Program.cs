95e7c576b1873f21c22540f8313d734c339e3363
MSchwarzer-Haverbier <Michael.Schwarzer-Haverbier@ma-design.de>
2014-12-08 16:05:18 +0100

    ES. SequenceNumber wird mit ausgegeben

--- EventStore/source/ev.EventStore/ev.EventStore/Program.cs
+++ EventStore/source/ev.EventStore/ev.EventStore/Program.cs
@@ -11,20 +11,22 @@ namespace ev.EventStore
             Console.WriteLine("-- 1. Event --");
             var myEvent = new Event(1, "Event 1", "Payload 1");
             eventStore.Record(myEvent);
-            foreach (var @event in eventStore.Replay())
-            {
-                Console.WriteLine("ContextID:{0} - EventName:{1} - Payload:{2}", @event.ContextId + @event.EventName, @event.Payload);
-            }
+            PrintEvents(eventStore);
 
             Console.WriteLine("-- 2. Event --");
             var myEvent2 = new Event(2, "Event 2", "Payload 2");
             eventStore.Record(myEvent2);
-            foreach (var @event in eventStore.Replay())
-            {
-                Console.WriteLine("ContextID:{0} - EventName:{1} - Payload:{2}", @event.ContextId + @event.EventName, @event.Payload);
-            }
+            PrintEvents(eventStore);
 
             Console.ReadLine();
+        }
+
+        private static void PrintEvents(EventStore eventStore)
+        {
+            foreach (var @event in eventStore.Replay())
+            {
+                Console.WriteLine("SequenceNumber:{0} - ContextID:{1} - EventName:{2} - Payload:{3}", @event.SequenceNumber, @event.ContextId + @event.EventName, @event.Payload);
+            }
         }
     }
 }
