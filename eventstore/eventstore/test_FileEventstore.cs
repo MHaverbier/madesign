1b26f68c3b6fc1896e344caf5b3d12b7965f1d84
Ralf Westphal <info@ralfw.de>
2014-12-08 15:25:10 +0100

    Refactoring

--- eventstore/eventstore/test_FileEventstore.cs
+++ eventstore/eventstore/test_FileEventstore.cs
@@ -21,6 +21,7 @@ namespace eventstore
 			Assert.IsTrue (File.Exists ("teststore/000000001.txt"));
 		}
 
+
 		[Test]
 		public void Replay()
 		{
@@ -47,45 +48,4 @@ namespace eventstore
 			Assert.AreEqual ("42", events [1].Payload);		
 		}
 	}
-
-
-
-	public class FileEventstore {
-		string dirpath;
-
-		public FileEventstore(string dirpath) {
-			this.dirpath = dirpath;
-			Directory.CreateDirectory (dirpath);
-		}
-
-		public void Record(Event e) {
-			e.Sequencenumber = Directory.GetFiles (this.dirpath).Length + 1;
-			var filename = Path.Combine (this.dirpath, string.Format ("{0:000000000}.txt", e.Sequencenumber));
-			using (var sw = new StreamWriter (filename)) {
-				sw.WriteLine (e.Sequencenumber);
-				sw.WriteLine (e.ContextId);
-				sw.WriteLine (e.Name);
-				sw.Write (e.Payload);
-			}
-		}
-
-		public IEnumerable<Event> Replay() {
-			var eventfilepaths = Directory.GetFiles (this.dirpath);
-			foreach (var efp in eventfilepaths) {
-				var serializedEvent = File.ReadAllLines (efp);
-				yield return Replay (serializedEvent);
-			}
-		}
-
-		Event Replay(string[] serializedEvent) {
-			var e = new Event ();
-			e.Sequencenumber = long.Parse (serializedEvent [0]);
-			e.ContextId = serializedEvent [1];
-			e.Name = serializedEvent [2];
-			e.Payload = string.Join ("\n", serializedEvent.Skip (3));
-			return e;
-		}
-	}
-
 }
-
