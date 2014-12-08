d62732b9861cec29afe9243cb4b31684b14c77b2
FelixHarder <flix.harder@gmail.com>
2014-12-08 16:01:36 +0100

    Record liefert Sequenznummer und Implementierung

--- EventStore/source/ev.EventStore/ev.EventStore/EventStore.cs
+++ EventStore/source/ev.EventStore/ev.EventStore/EventStore.cs
@@ -1,17 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ev.EventStore
 {
     public class EventStore
     {
-        public void Record(Event evt)
-        {
+        private int aktuelleSequenzNummer;
 
+        public int Record(Event evt)
+        {
+            var neueSequenzNummer = aktuelleSequenzNummer + 1;
+            
+            var dateiName = string.Format("Event-{0:000000000000}", neueSequenzNummer);
+
+            using (var datei =  new StreamWriter(dateiName))
+            {
+                datei.WriteLine(neueSequenzNummer);
+                datei.WriteLine(evt.ContextId);
+                datei.WriteLine(evt.EventName);
+                datei.Write(evt.Payload);
+            }
+
+            aktuelleSequenzNummer = neueSequenzNummer;
+            return neueSequenzNummer;
         }
 
         public IEnumerable<Event> Replay()
         {
+        
             yield break;
         }
     }
