49960793307c1d2c42491027c2781b8f52b32682
ClaudiaLange <claudia.lange@ma-design.de>
2014-12-08 15:14:58 +0100

    EventStore angelegt

---
+++ EventStore/source/ev.EventStore/ev.EventStore/EventStore.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace ev.EventStore
+{
+    public class EventStore
+    {
+        public void Record(Event evt)
+        {
+
+        }
+
+        public IEnumerable<Event> Replay()
+        {
+            yield break;
+        }
+    }
+}
