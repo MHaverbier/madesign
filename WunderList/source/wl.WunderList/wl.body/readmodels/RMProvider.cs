1075c3e7a012f702abba9c9adb1259d46780c9fc
FelixHarder <flix.harder@gmail.com>
2015-02-09 14:08:11 +0100

    WL: Update RMProvider API

--- WunderList/source/wl.WunderList/wl.body/readmodels/RMProvider.cs
+++ WunderList/source/wl.WunderList/wl.body/readmodels/RMProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,22 +9,46 @@ namespace wl.body.readmodels
 {
     public class RMProvider
     {
+        string filename;
 
-
-        public bool IsInitialized()
+        public RMProvider(string dirpath)
         {
-            return false;
+            this.filename = Path.Combine(dirpath, "ReadModel.txt");
+            Directory.CreateDirectory(dirpath);
+        }
+
+        public void IsInitialized(Action OnInitialized, Action OnNotIntialized)
+        {
+            if (File.Exists(this.filename))
+            {
+                OnInitialized();
+            }
+            else
+            {
+                OnNotIntialized();
+            }
         }
 
         public void Persist(IEnumerable<dynamic> tms)
         {
-
+            using (var sw = new StreamWriter(filename))
+            {
+                foreach (var tm in tms)
+                {
+                    sw.WriteLine(String.Format("{},{},{}", tm.Id, tm.Name, tm.NumberOfTasks));
+                }
+            }
         }
 
-        public IEnumerable<dynamic> Depersist()
+        public IEnumerable<dynamic> DePersist()
+        {
+            var serializedRM = File.ReadAllLines(this.filename);
+            yield return null;
+        }
+
+        dynamic CreateTM(string serializedTM)
         {
             return null;
         }
-
     }
 }
