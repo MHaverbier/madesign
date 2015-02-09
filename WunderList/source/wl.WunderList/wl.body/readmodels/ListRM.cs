ebe2321f4bb22a6b099e569648f1fff242bd37d3
FelixHarder <flix.harder@gmail.com>
2015-02-09 16:19:08 +0100

    WL: Add ListRM

---
+++ WunderList/source/wl.WunderList/wl.body/readmodels/ListRM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wl.body.readmodels
+{
+    class ListRM
+    {
+        public ListRM(string listId, string listName)
+        {
+            ListId = listId;
+            ListName = listName;
+            TaskIds = new List<TaskRm>();
+        }
+
+        public struct TaskRm
+        {
+            public string TaskId { get; set; }
+            public bool IsActive { get; set; }
+        }
+
+        public string ListId { get; private set; }
+
+        public string ListName { get; private set; }
+
+        public List<TaskRm> TaskIds { get; private set; }
+    }
+}
