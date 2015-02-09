eb40706d9401fac773ae331d006937ce8f55d682
ClaudiaLange <claudia.lange@ma-design.de>
2015-02-09 14:50:43 +0100

    WL: Test für ReadModelBuilder
    Tests erzeugt für ReadModelBuilder.

---
+++ WunderList/source/wl.WunderList/wl.body.Test/EventExtensions.cs
@@ -0,0 +1,21 @@
+﻿using eventstore;
+
+namespace wl.body.Test
+{
+    public static class EventExtensions
+    {
+        public static long sequenceNumber = 0L;
+
+        public static void Reset()
+        {
+            sequenceNumber = 0L;
+        }
+
+        public static Event Create(this string @eventName, string contextId, string payload = null)
+        {
+            sequenceNumber++;
+            return new Event{ContextId = contextId, Name = @eventName, Sequencenumber = sequenceNumber, Payload = payload};
+
+        }
+    }
+}
---
+++ WunderList/source/wl.WunderList/wl.body.Test/ReadModelBuilderTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using eventstore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using wl.body.readmodels;
+
+namespace wl.body.Test
+{
+    [TestClass]
+    public class ReadModelBuilderTest
+    {
+        
+        [TestMethod]
+        public void ListCreated_CreatesASingle_Tm()
+        {
+            var builder = new RMBuilder();
+
+            var @events = new[] {new Event() {ContextId = "1", Name = "ListCreated", Payload = "Listen Name", Sequencenumber = 0L}};
+            var result = builder.Build(@events).ToArray();
+
+            Assert.AreEqual(1, result.Length);
+        }
+
+        [TestMethod]
+        public void OnlyActivatedTasksAreCounted()
+        {
+            var builder = new RMBuilder();
+
+            var @events = new[]
+            {
+                "ListCreated".Create("1", "Listen Name"),
+                "TaskAddedToList".Create("1", "123" ),
+                "TaskAddedToList".Create("1", "4711" ),
+                "TaskAddedToList".Create("1", "42" ),
+                "TaskActivated".Create("4711"),
+                "TaskActivated".Create("123"),
+                "TaskDeactivated".Create("4711"),
+                "TaskActivated".Create("42"),
+            };
+            var result = builder.Build(@events).ToArray();
+
+            Assert.AreEqual(1, result.Length, "Expected one list item");
+            Assert.AreEqual("1", result[0].Id, "List Id mismatch");
+            Assert.AreEqual("Listen Name", result[0].Name, "List Name mismatch");
+            Assert.AreEqual(2, result[0].NumberOfTasks, "Activated task mismatch.");
+        }
+    }
+}
